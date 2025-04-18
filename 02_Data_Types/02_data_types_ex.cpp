47640d87c48cae7d4dea947836d22d507f509147
Sagar <sagarmore1007@gmail.com>
2025-04-18 11:30:01 +0530

    Data Types

---
+++ 02_Data_Types/02_data_types_ex.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Integer data type
+    int age = 25;
+    cout << "Age (int): " << age << endl;
+
+    // Floating point data type
+    float height = 5.9;
+    cout << "Height (float): " << height << endl;
+
+    // Double data type (more precision than float)
+    double pi = 3.1415926535;
+    cout << "Value of Pi (double): " << pi << endl;
+
+    // Character data type
+    char grade = 'A';
+    cout << "Grade (char): " << grade << endl;
+
+    // Boolean data type
+    bool isPassed = true;
+    cout << "Is Passed (bool): " << isPassed << endl;  // Output: 1 (true)
+
+    // String using C++ string library
+    string name = "Sagar";
+    cout << "Name (string): " << name << endl;
+
+    return 0;
+}
